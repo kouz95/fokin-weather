38b73f4fb5fb032a99292c8f0fb19d8dd332f886
kouz <lastpuzly@gmail.com>
2020-07-03 19:25:21 +0900

    feat: add getWeather, use axios

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import * as Location from 'expo-location';
 import { Alert } from 'react-native';
 import Loading from './src/Loading';
+import axios from 'axios';
 
 interface Props {}
 
 interface State {
   isLoading: boolean;
 }
+
+const API_KEY = '4c4fd33dc52199132ebbcbb788b5711e';
 
 export default class extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -22,12 +25,22 @@ export default class extends React.Component<Props, State> {
     this.getLocation();
   }
 
+  getWeather = async (latitude: number, longitude: number) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+
+    console.log(data);
+  };
+
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false })
     } catch (error) {
       Alert.alert("Can't find you.", 'So sad');
     }
