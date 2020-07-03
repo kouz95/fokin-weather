9ed59687edb12dd11f7fd8424ad1dac4a252b85b
kouz <lastpuzly@gmail.com>
2020-07-03 17:58:34 +0900

    feat: use State, resolve #2

--- App.tsx
+++ App.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
-import Loading from "./src/Loading";
+import * as Location from 'expo-location';
+import { Alert } from 'react-native';
+import Loading from './src/Loading';
 
-export default function App() {
-  return (
-    <Loading />
-  );
+interface Props {}
 
+interface State {
+  isLoading: boolean;
+}
+
+export default class extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+    };
+  }
+
+  componentDidMount(): void {
+    this.getLocation();
+  }
+
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+    } catch (error) {
+      Alert.alert("Can't find you.", 'So sad');
+    }
+  };
+
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
